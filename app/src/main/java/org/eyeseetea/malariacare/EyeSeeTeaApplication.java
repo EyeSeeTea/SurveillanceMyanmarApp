6ae6d088c55cf6d4364452c78a0a579e86c10b6c
ifoche <nacho.foche@gmail.com>
2018-03-30 10:30:53 +0200

    point to Myanmar blessed repository

--- app/src/main/java/org/eyeseetea/malariacare/EyeSeeTeaApplication.java
+++ app/src/main/java/org/eyeseetea/malariacare/EyeSeeTeaApplication.java
@@ -85,7 +85,7 @@ public class EyeSeeTeaApplication extends Application {
                 .setLoggingEnabled(BuildConfig.DEBUG)
                 .setAlertDialogType(AlertDialogType.APP_COMPAT)
                 .setGitHubInfo(new GitHubConfiguration(
-                        "eyeseetea/pictureapp",
+                        "eyeseetea/SurveillanceMyanmarApp",
                         BuildConfig.GIT_HUB_BOT_TOKEN,
                         "eyeseeteabottest/snapshots",
                         "master"))
